334e828b0e57a2075ccb6c577fbe952809059618
Elias Eichen <eliprof@live.com>
2021-06-02 11:40:07 -0500

    added generator model

---
+++ app/models/generator.py
@@ -0,0 +1,22 @@
+from .db import db
+
+
+class Generator(db.Model):
+    __tablename__ = 'generators'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(40), nullable=False)
+    seed = db.Column(db.Integer, nullable=False)
+    iteration = db.Column(db.Integer, default=0)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+    user = db.relationship('User', back_populates='generator')
+
+    def to_dict(self):
+        return {
+            id: self.id,
+            title: self.title,
+            seed: self.seed,
+            iteration: self.iteration
+            user_id: self.user_id
+        }
