189ec20e0e8a5871cb9b46c2c4dd24527faf87e7
Elias Eichen <eliprof@live.com>
2021-06-09 12:18:47 -0500

    added hover hint for pwlength constriant

--- react-app/src/components/constraintsDisplay/PWLength.js
+++ react-app/src/components/constraintsDisplay/PWLength.js
@@ -5,12 +5,15 @@ const PasswordLength = ({pwLength, setPwLength}) =>{
     return (
         <div className='pw-len'>
             <div>Password Length</div>
-            <input
-            className='pw-len-input' 
-            type="number"
-            value={pwLength}
-            onChange={e => setPwLength(e.target.value)}
-            />
+            <div className='hover-hint'>
+                <input
+                className='pw-len-input' 
+                type="number"
+                value={pwLength}
+                onChange={e => setPwLength(e.target.value)}
+                />
+                <span className='hint pw-hint'>sets the length of the password, note that the total required characters cannot exceed the password length</span>
+            </div>
         </div>
     )
 }
