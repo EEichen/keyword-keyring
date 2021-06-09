3b99e3e8db0da79813657f5404923a230c95bbba
Elias Eichen <eliprof@live.com>
2021-06-09 15:02:08 -0500

    iterator hint added and hint issues fixed

--- react-app/src/components/generatorsDisplay/Iterator.js
+++ react-app/src/components/generatorsDisplay/Iterator.js
@@ -2,11 +2,14 @@ import React from 'react'
 
 const Iterator = ({iteration, increment, decrement}) => {
     return(
-        <div className='iteration'>
-            <div className='iterator-label'>Iterator:</div>
-            <button className='decrement' onClick={decrement}>-</button>
-            <div className='iteration-display'>{iteration}</div>
-            <button className='increment' onClick={increment}>+</button>
+        <div className='hover-hint'>
+            <div className='iteration'>
+                <div className='iterator-label'>Iterator:</div>
+                <button className='decrement' onClick={decrement}>-</button>
+                <div className='iteration-display'>{iteration}</div>
+                <button className='increment' onClick={increment}>+</button>
+            </div>
+           <span className={`hint itr-hint`}>the iterator changes the password generated without creating a new generator</span>
         </div>
     )
 }
