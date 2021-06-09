bd049ad90e68b89f7e26a405c7c6a20d3457c8f9
Elias Eichen <eliprof@live.com>
2021-06-09 12:06:26 -0500

    hover hints for allowed characters added

--- react-app/src/components/constraintsDisplay/AllowedCharacters.js
+++ react-app/src/components/constraintsDisplay/AllowedCharacters.js
@@ -15,43 +15,56 @@ const AllowedCharacters = ({
         <div className='allowed-characters'>
             <h4>Allowed characters</h4>
             <div>
-            <label htmlFor="lowercase">Lowercase</label>
-            <input 
-            id='lowercase'
-            type="text" 
-            value={lowercase}
-            onChange={e => setLowercase(e.target.value)}
-            />
+                <label htmlFor="lowercase">Lowercase</label>
+                <div className='hover-hint'>
+                    <input  
+                    id='lowercase'
+                    type="text" 
+                    value={lowercase}
+                    onChange={e => setLowercase(e.target.value)}
+                    />
+   
+                    <span className='hint allow-hint'>sets the lowercase letters allowed in the password</span>
+                </div>
+         </div>
+
+            <div>
+                <label htmlFor="uppercase">Uppercase</label>
+                <div className='hover-hint'>
+                    <input
+                        id='uppercase'
+                        type="text"
+                        value={uppercase}
+                        onChange={e => setUppercase(e.target.value)}
+                        />
+                    <span className='hint allow-hint'>sets the uppercase letters allowed in the password</span>
+                </div>
             </div>
 
             <div>
-            <label htmlFor="uppercase">Uppercase</label>
-            <input
-                id='uppercase'
-                type="text"
-                value={uppercase}
-                onChange={e => setUppercase(e.target.value)}
-                />
+                <label htmlFor="numbers">Numbers</label>
+                <div className='hover-hint'>
+                    <input
+                        id='numbers'
+                        type="text"
+                        value={numbers}
+                        onChange={e => setNumbers(e.target.value)}
+                        />
+                    <span className='hint allow-hint'>sets the numbers allowed in the password</span>
+                </div>
             </div>
 
             <div>
-            <label htmlFor="numbers">Numbers</label>
-            <input
-                id='numbers'
-                type="text"
-                value={numbers}
-                onChange={e => setNumbers(e.target.value)}
-                />
-            </div>
-
-            <div>
-            <label htmlFor="symbols">Symbols</label>
-            <input
-                id='symbols'
-                type="text"
-                value={symbols}
-                onChange={e => setSymbols(e.target.value)}
-                />
+                <label htmlFor="symbols">Symbols</label>
+                <div className='hover-hint'>
+                    <input
+                        id='symbols'
+                        type="text"
+                        value={symbols}
+                        onChange={e => setSymbols(e.target.value)}
+                        />
+                    <span className='hint allow-hint'>sets the symbols allowed in the password</span>
+                </div>
             </div>
         </div>
     )
