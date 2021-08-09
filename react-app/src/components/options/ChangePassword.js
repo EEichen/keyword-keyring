5e0aa0d24f1b1381fa6d59d92bedfcf143d5fdfb
Elias Eichen <eliprof@live.com>
2021-08-09 14:47:04 -0500

    work on cp component

--- react-app/src/components/options/ChangePassword.js
+++ react-app/src/components/options/ChangePassword.js
@@ -4,7 +4,9 @@ import React from 'react'
 const ChangePassword = () =>{
     return (
         <div>
-            
+            <input type="password" placeholder="Previous password" name="" id="" />
+            <input type="password" placeholder="New password" name="" id="" />
+            <input type="password" placeholder="Confirm new password" name="" id="" />
         </div>
     )
 }
