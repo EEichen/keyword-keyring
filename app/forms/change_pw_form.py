e123e541f34a57cb6ccdf90a3cd3e5b4d5b111fc
Elias Eichen <eliprof@live.com>
2021-08-12 16:14:24 -0500

    updated change pw component

--- app/forms/change_pw_form.py
+++
@@ -1,4 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField
-from wtforms.validators import DataRequired, Email, ValidationError
-from app.models import User
--- react-app/src/components/options/ChangePassword.js
+++ react-app/src/components/options/ChangePassword.js
@@ -2,11 +2,19 @@ import React from 'react'
 
 
 const ChangePassword = () =>{
+
+    const onConfirm = () => {
+        
+    }
+
     return (
-        <div>
+        <div className='change-pw'>
             <input type="password" placeholder="Previous password" name="" id="" />
             <input type="password" placeholder="New password" name="" id="" />
             <input type="password" placeholder="Confirm new password" name="" id="" />
+            <button>Confirm</button>
         </div>
     )
 }
+
+export default ChangePassword
