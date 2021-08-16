1004f806a7f00f43c7d9cc5714760535541100f4
Elias Eichen <eliprof@live.com>
2021-08-16 14:38:17 -0500

    started work on cp backend route

--- app/api/user_routes.py
+++ app/api/user_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from flask_login import login_required, current_user
 from app.models import User
 
@@ -8,4 +8,5 @@ user_routes = Blueprint('users', __name__)
 @user_routes.route('/', methods=['PUT'])
 @login_required
 def change_password():
-    pass
+    data = request.json()
+    user = current_user
